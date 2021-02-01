9501047c4126e5bde3182942e16d65bb7f0c05fd
Gabriel R. Santos <47397347+biandishilaji@users.noreply.github.com>
2021-01-31 19:48:08 -0500

    Update ServiceRepository.php

--- app/Infrastructure/Repositories/Modules/Management/ServiceRepository.php
+++ app/Infrastructure/Repositories/Modules/Management/ServiceRepository.php
@@ -9,6 +9,36 @@ Use App\Infrastructure\Repositories\Modules\Management\Interfaces\IServiceReposi
 
 class ServiceRepository extends BaseRepository implements IServiceRepository
 {
+    
+    public function getListServices($filter){
+        
+        $queryBuilder = $this->model;
+        
+        if ($filter){
+        foreach($filter as $name => $value){
+            
+//             $filter = [
+//                 'name' = 'TESTE'
+//                ];
+            
+            switch($name){
+                    
+                case 'name':
+                    $queryBuilder->where('name', 'LIKE', "%$value%");
+                    break;
+            }
+        }
+            
+            return $queryBuilder;
+    }
+        
+        public function getListServicesPaginate($filter, $itemsByPage){
+        
+        $queryBuilder = $this->getListServices($filter);
+            
+        return $queryBuilder->paginate($itemsByPage);
+        }
+    
     function model()
     {
         return Service::class;
